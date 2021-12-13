ef6a6a64fea1b795aa3dfb5b7aa5e2c2010c31bd
Matheus Tanaka <matheustanakanog@gmail.com>
2021-12-13 19:26:07 -0300

    feat: import cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const TaskRouter = require("./src/routes/task.routes")
 
 const connectToDatabase = require("./src/database/mongoose.database");
@@ -7,6 +8,7 @@ const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectToDatabase();
 
